72ac5a019bd6c08caf80a20b0a6d3a08ee3736a8
matt <emal>
2023-04-04 11:50:56 -0500

    Video 4. Added clear form, hide keyboard, add places, duplicate restrictions

--- app/src/main/java/com/example/travelwishlist/MainActivity.kt
+++ app/src/main/java/com/example/travelwishlist/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.example.travelwishlist
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
+import android.widget.LinearLayout
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
@@ -12,6 +18,12 @@ class MainActivity : AppCompatActivity() {
     private lateinit var addNewPlaceButton: Button
     // RecyclerView
     private lateinit var placeListRecyclerView: RecyclerView
+    // Reference to the RecyclerView
+    private lateinit var placesRecyclerAdapter: PlaceRecyclerAdapter
+    // Reference to the ViewModel, only going to get used when call - lazy
+    private val placesViewModel: PlacesViewModel by lazy {
+        ViewModelProvider(this).get(PlacesViewModel::class.java)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +33,52 @@ class MainActivity : AppCompatActivity() {
         addNewPlaceButton = findViewById(R.id.add_new_place_button)
         newPlaceEditText = findViewById(R.id.new_place_name)
 
+        // Asking ViewModel for a list of places
+        val places = placesViewModel.getPlaces()
+
+        // Setting up the RecyclerView. Data is from places list in PlacesRecyclerAdapter.kt
+        placesRecyclerAdapter = PlaceRecyclerAdapter(places)
+        // View Component
+        placeListRecyclerView.layoutManager = LinearLayoutManager(this)
+        placeListRecyclerView.adapter = placesRecyclerAdapter
+
         addNewPlaceButton.setOnClickListener {
             addNewPlace()
         }
     }
 
     private fun addNewPlace() {
-        // TODO
+        // Reads the data from the editText and adding it to the list
+        val name = newPlaceEditText.text.toString().trim()
+        if (name.isEmpty()) {
+            Toast.makeText(this, "Enter a place name", Toast.LENGTH_SHORT).show()
+        } else {
+            // Modifies the placesViewModel with new data
+            val positionAdded = placesViewModel.addNewPlace(name)
+
+// Checking if position was added in PlacesViewModel.kt
+            if (positionAdded == -1) {
+                Toast.makeText(this, "You already added that place.", Toast.LENGTH_SHORT).show()
+            } else {
+            // Telling the RecyclerAdapter the list has changed
+                placesRecyclerAdapter.notifyItemInserted(positionAdded)
+                // Calling Clear Form function to clear all the text in the textBox
+                clearForm()
+                // Function that makes the keyboard go away to prevent typing
+                hideKeyBoard()
+            }
+        }
+    }
+
+    private fun clearForm() {
+        newPlaceEditText.text.clear()
+    }
+
+    private fun hideKeyBoard() {
+        this.currentFocus?.let { view ->            // Current focus = what is the user typing in?
+            val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+        // imm = "Input Method Manager"
+           imm?.hideSoftInputFromWindow(view.windowToken, 0)
+        }
     }
 }
--- app/src/main/java/com/example/travelwishlist/PlacesViewModel.kt
+++ app/src/main/java/com/example/travelwishlist/PlacesViewModel.kt
@@ -5,11 +5,41 @@ import androidx.lifecycle.ViewModel
 class PlacesViewModel: ViewModel() {
 
     // Private to prevent duplicate placenames
-    private val placeNames = mutableListOf<String>("Brainerd", "Custer")
+    private val placeNames = mutableListOf<String>("Brainerd", "Custer", "Minneapolis", "Mystic")
 
     // function to access the list
     fun getPlaces(): List<String> {
         return placeNames           // Smart cast to convert mutable list to list
     }
 
+    fun addNewPlace(place: String, position: Int? = null): Int {         // Nullable Integer = ?
+
+        // Avoid duplicates
+/*
+    // Duplicate loop check
+        for (placeName in placeNames) {
+            if (placeName.uppercase() == place.uppercase()) {
+                return - 1          // -1 indicates a place was not added
+            }
+        }
+ */
+        // Kotlin list function "any" or "all"
+            // 'all' returns true if all of the list items meet this condition
+            // 'any' returns true if any of the placeName list items meet this condition
+        if(placeNames.any { placeName -> placeName.uppercase() == place.uppercase() } ) {
+            return -1       // Indicates 'not added'
+        }
+        // Implement add at position
+        return if (position == null) {      // If no position is given
+            // return location in the list that the new item was added
+            placeNames.add(place)       // Basic version adds to the end of the list
+            placeNames.lastIndex
+        } else {
+            placeNames.add(position, place)
+            position
+        }
+
+
+
+    }
 }
