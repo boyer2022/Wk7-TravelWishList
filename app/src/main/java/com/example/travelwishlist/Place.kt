eb85cb879167e094cc27152f8b5910b802fe7896
matt <emal>
2023-04-08 10:46:04 -0500

    Added Date formatter patterns link as comment

--- app/src/main/java/com/example/travelwishlist/Place.kt
+++ app/src/main/java/com/example/travelwishlist/Place.kt
@@ -6,6 +6,7 @@ import java.util.*
 // Constructor()
 class Place(val name: String, val dateAdded: Date = Date()) {
     fun formattedDate(): String {
+        // Date formatter patterns:  https://docs.oracle.com/javase/10/docs/api/java/text/SimpleDateFormat.html
         return SimpleDateFormat("EEE, d MMM yyyy", Locale.getDefault()).format(dateAdded)
     }
 }
